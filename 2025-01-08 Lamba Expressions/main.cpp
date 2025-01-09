d88a7a91646f1968cab0c5d3433cd78d4c2966d2
JettM9104 <64600882+JettM9104@users.noreply.github.com>
2025-01-09 12:20:26 -0500

    asdfasddfs

---
+++ 2025-01-08 Lamba Expressions/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// lambda expressions can be declared inside a function and gets deleted outside the enclosing scope
+// another advantage is you can use the auto keyword, acutally you have to use the auto keyword
+// if you want to declare the type, after the parameter list () you can add  -> int, for example for the return type to be int.
+// as seen below
+
+
+int main() {
+  auto add = [](int a, int b) -> int {
+    return a + b;
+  };
+}
