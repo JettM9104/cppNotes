a81a2dc951238e8f189dc37f6c2f88d5eb7a9da6
JettM9104 <64600882+JettM9104@users.noreply.github.com>
2025-01-09 12:10:18 -0500

    a

--- 2025-01-07 Modifiers/main.cpp
+++ 2025-01-07 Modifiers/main.cpp
@@ -27,6 +27,11 @@ public:
   constexpr int method5(int d) { // This function will evaluate this during compile time but there are limitations
     return g * 9;
   }
+
+  friend int method6(int k); // Functinos decladed freinds can be define outsiode of the class and still have acccess to private and public variables.
+
+  friend int method7(base& hi);
+
   base(int a); // You have to declare the constructor if you want to write it outside the class
 
   base (double a) { // constructor
@@ -51,6 +56,14 @@ private:
 
 
 };
+int method6(int k) { // This is a reguaar Functino that has access to base class
+  return k * 20;
+  // If you want it to make the variables accesible you will need to make a refrence of the base class
+}
+
+int method7(base& hi) {
+  return hi.a;
+}
 
 base::base(int a) { // constructor outside of the functino
   this->a = a;
